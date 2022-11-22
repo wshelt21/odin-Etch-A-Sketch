0de296f4e015c5f60f8e01f8cc0a0f7cdb071a1c
wshelt21 <william.shelton24@gmail.com>
2022-11-22 09:21:35 -0500

    add reset button, more styles

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const grid = document.querySelector('.grid-container');
-
+const resetButton = document.querySelector('button');
 
 
 // Grid Function
 const createGrid = (gridNum) =>{
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('wrapper');
     for(let i = 0; i < gridNum; i++){
         const row = document.createElement('div');
         row.classList.add('grid-row');
 
 
     for(let j = 0; j < gridNum; j++){
-        const widthAndHeight = (500 / gridNum)-2;
+        const widthAndHeight = (960 / gridNum)-2;
         const cell = document.createElement('div');
         cell.classList.add('cell');
         cell.style.width = `${widthAndHeight}px`;
@@ -22,7 +24,19 @@ const createGrid = (gridNum) =>{
         row.appendChild(cell);
     }
 
-    grid.appendChild(row);
+    wrapper.appendChild(row);
     }
+    grid.appendChild(wrapper)
 }
+
 createGrid(16);
+
+resetButton.addEventListener('click', ()=>{
+    let userSize = Number(prompt('Select a Grid Dimenison Size:'));       
+    while(userSize > 100){
+        userSize = Number(prompt('Please pick a size no greater than 100:'));
+    }
+    const wrapper = document.querySelector('.wrapper');
+    wrapper.remove();
+    createGrid(userSize);
+})
